64b7ae30e3ad1c443bcffc615e2daae4507439df
paullessing <paul@paullessing.com>
2021-06-18 19:32:40 +0100

    Add MQTT handler and dotenv config

---
+++ src/temperature-poller.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
+import { MqttClient } from './mqtt';
+import { Temperatures } from './temperatures.model';
+
+export class TemperaturePoller {
+  constructor(
+    private mqttClient: MqttClient,
+    private temperatures$: Observable<Temperatures>
+  ) {}
+
+  public async startPolling(): Promise<void> {
+    await this.mqttClient.setupDiscovery();
+
+    let dataCount = 0;
+    return new Promise((_, reject) => {
+      this.temperatures$.pipe(
+        throttleTime(5000),
+      ).subscribe((temps) => {
+        if (dataCount === 0) {
+          console.log('Data connected');
+        }
+        dataCount++;
+        if (dataCount % 12 === 0) {
+          process.stdout.write(dataCount % 10000 === 0 ? ',' : '.');
+        }
+
+        this.mqttClient.publishState(temps);
+      }, reject);
+    });
+  }
+}
